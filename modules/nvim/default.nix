5ad585e427222b8bf35f90fe5d9f872521824d1e
nasir <nasirkhimani@gmail.com>
2023-09-03 17:32:52 -0400

    rust

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -71,6 +71,12 @@ in
             -- (Optional) Configure lua language server for neovim
             require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
             lsp.setup()
+            local cmp = require('cmp')
+            cmp.setup({
+                mapping = {
+                    ['<CR>'] = cmp.mapping.confirm({select = false}),
+                }
+            })
             EOF
           '';
         }
