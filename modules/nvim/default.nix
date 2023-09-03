ba22744bb0bf109745c79f5d98a8a0c2cd67816f
nasir <nasirkhimani@gmail.com>
2023-09-03 17:43:34 -0400

    Revert "rust"
    This reverts commit ecec3c9da50550a543f70049fcaec5083b41317e.

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -61,7 +61,7 @@ in
           plugin = nvim-lsp-zero;
           config = ''
             lua << EOF
-            local lsp = require('lsp-zero')
+            local lsp = require('lsp-zero').preset("recommended")
             lsp.on_attach(function(client, bufnr)
                 -- see :help lsp-zero-keybindings
                 -- to learn the available actions
@@ -70,6 +70,7 @@ in
             lsp.setup_servers({'lua_ls', 'rnix', 'rust_analyzer'})
             -- (Optional) Configure lua language server for neovim
             require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+            lsp.setup()
             local cmp = require('cmp')
             cmp.setup({
                 mapping = {
