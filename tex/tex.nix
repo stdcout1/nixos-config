f10a5a12688066e86f0d0722f278c693f1350777
nasir <nasirkhimani@gmail.com>
2024-03-06 18:25:11 -0500

    tex additions

---
+++ tex/tex.nix
@@ -0,0 +1,15 @@
+# im not a power user so this suffices 
+with import <nixpkgs> {};
+pkgs.mkShell {
+    packages = with pkgs; [
+        (texliveMedium.withPackages (ps: with ps; [ listings xcolor tcolorbox pgf environ ]))
+        texmaker
+    ];
+    #usefull for spell check in texmaker need to manually set tough
+    shellHook = ''
+        mkdir ./dictionaries
+        curl https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_CA.dic > ./dictionaries/en_CA.dic
+        curl https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_CA.aff > ./dictionaries/en_CA.aff
+    '';
+}
+
