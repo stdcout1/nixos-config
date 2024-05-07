3bf6e5b0149f6c82505ee8d4d0da0de5746a5ebf
nasir <nasirkhimani@gmail.com>
2024-05-07 16:58:18 -0400

    new rofi config

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -1,21 +1,24 @@
-{ inputs, pkgs, lib, config, ...}: 
+{ inputs, pkgs, lib, config, ... }:
 
 with lib;
 
 let
-    cfg = config.modules.hyprland;
-    swww-randomize = pkgs.writeShellScriptBin "swww-randomize" ''${builtins.readFile ./swww-randomize.sh}'';
-in { 
+  cfg = config.modules.hyprland;
+  swww-randomize = pkgs.writeShellScriptBin "swww-randomize" ''${builtins.readFile ./swww-randomize.sh}'';
+in
+{
   options.modules.hyprland = { enable = mkEnableOption "hyprland"; };
-  options.desktop = {enable = mkEnableOption "desktop"; }; #enable desktop mode for hyprland
+  options.desktop = { enable = mkEnableOption "desktop"; }; #enable desktop mode for hyprland
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
-      rofi-wayland swww wl-clipboard swww-randomize
+      swww
+      wl-clipboard
+      swww-randomize
     ];
     wayland.windowManager.hyprland = {
-        enable = true;
-        systemd.enable = true;
-        extraConfig = builtins.readFile (if config.desktop.enable then ./hyprland_desktop.conf else ./hyprland.conf);
+      enable = true;
+      systemd.enable = true;
+      extraConfig = builtins.readFile (if config.desktop.enable then ./hyprland_desktop.conf else ./hyprland.conf);
     };
     #home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
   };
