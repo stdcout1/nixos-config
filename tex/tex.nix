9e72f7f1509f645404a382ba53c42165cde0bd3a
nasir <nasirkhimani@gmail.com>
2024-10-14 14:51:45 -0400

    changed to texstudio

--- tex/tex.nix
+++ tex/tex.nix
@@ -1,15 +1,18 @@
 # im not a power user so this suffices 
-with import <nixpkgs> {};
+with import <nixpkgs> { };
 pkgs.mkShell {
-    packages = with pkgs; [
-        (texliveMedium.withPackages (ps: with ps; [ listings xcolor tcolorbox pgf environ ]))
-        texmaker
-    ];
-    #usefull for spell check in texmaker need to manually set tough
-    shellHook = ''
+  packages = with pkgs; [
+    (texliveMedium.withPackages (ps: with ps; [ listings xcolor tcolorbox pgf environ ]))
+    texstudio
+  ];
+  #usefull for spell check in texmaker need to manually set tough
+  shellHook = ''
+    if [ ! -d "./dictionaries" ]; then
         mkdir ./dictionaries
         curl https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_CA.dic > ./dictionaries/en_CA.dic
         curl https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_CA.aff > ./dictionaries/en_CA.aff
-    '';
+    fi
+    texstudio
+  '';
 }
 
