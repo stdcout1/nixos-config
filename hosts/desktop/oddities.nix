391c1ae258c44cceafda20d522d2c9e49ebf4023
nasir <nasirkhimani@gmail.com>
2025-02-09 02:42:20 -0500

    updated desktop

--- hosts/desktop/oddities.nix
+++ hosts/desktop/oddities.nix
@@ -12,10 +12,8 @@
   services.logrotate.checkConfig = false;
   boot.kernelParams = [ "module_blacklist=i915" ]; # blacklist integrated gpu
   # Enable OpenGL
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
     extraPackages = [ pkgs.nvidia-vaapi-driver ];
   };
 
@@ -62,29 +60,7 @@
     # package = config.boot.kernelPackages.nvidiaPackages.production;
 
     # Special config to load the latest (535 or 550) driver for the support of the 4070 SUPER
-    package =
-      let
-        rcu_patch = pkgs.fetchpatch {
-          url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-          hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-        };
-      in
-      config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "555.42.02";
-        sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
-        sha256_aarch64 = lib.fakeSha256;
-        openSha256 = "sha256-3/eI1VsBzuZ3Y6RZmt3Q5HrzI2saPTqUNs6zPh5zy6w=";
-        settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-        persistencedSha256 = lib.fakeSha256;
-        #version = "550.40.07";
-        #sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
-        #sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
-        #openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
-        #settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
-        #persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
-
-        # patches = [ rcu_patch ];
-      };
-  };
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    };
 }
 
