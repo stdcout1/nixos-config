e43854abac2b2eb1222be92544df22a9fcdd81f6
nasir <nasirkhimani@gmail.com>
2025-06-17 17:04:03 -0400

    changed to a minimal waybar

--- modules/waybar/default.nix
+++ modules/waybar/default.nix
@@ -12,9 +12,99 @@ in
     home.packages = [ adthand ];
     programs.waybar = {
       enable = true;
-      package = pkgs.waybar.overrideAttrs (oldAttrs: { mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ]; });
+      settings = {
+        mainbar = {
+          layer = "top";
+          margin-bottom = -2;
+          position = "top";
+          height = 26;
+          output = [
+            "eDP-1"
+          ];
+
+          modules-left = [ "custom/logo" "hyprland/workspaces" ];
+          modules-right = [ "custom/adthand" "network" "clock" "battery" ];
+
+          "custom/logo" = {
+            format = "";
+            tooltip = false;
+            on-click = '''';
+          };
+          "custom/adthand" = {
+            format = "{}";
+            return-type = "json";
+            tooltip = true;
+            exec = "adthand waybar";
+            interval = 60;
+            max-length = 30;
+          };
+
+          "hyprland/workspaces" = {
+            on-click = "activate";
+          };
+
+          "clock" = {
+            interval = 60;
+            format = "{:%a %d/%m %I:%M}";
+          };
+
+          "battery" = {
+            tooltip = false;
+          };
+
+          "network" = {
+            format = "{essid}";
+          };
+        };
+      };
+      style = ''
+        * {
+          border: none;
+          border-radius: 0;
+          padding: 0;
+          margin: 0;
+          font-size: 11px;
+        }
+
+        window#waybar {
+          background: #292828;
+          color: #ffffff;
+        }
+  
+        #custom-logo {
+          font-size: 18px;
+          margin: 0;
+          margin-left: 7px;
+          margin-right: 12px;
+          padding: 0;
+          font-family: JetBrainsMono Nerd Font Mono;
+        }
+
+        #custom-adthand {
+            margin-right: 7px
+        }
+  
+        #workspaces button {
+          margin-right: 10px;
+          color: #ffffff;
+        }
+        #workspaces button:hover, #workspaces button:active {
+          background-color: #292828;
+          color: #ffffff;
+        }
+        #workspaces button.active {
+          background-color: #383737;
+        }
+
+        #network {
+          margin-right: 7px;		
+        }
+
+        #battery {
+          margin-left: 7px;
+          margin-right: 3px;
+        }
+      '';
     };
-    home.file.".config/waybar/config".source = ./config;
-    home.file.".config/waybar/style.css".source = ./style.css;
   };
 }
